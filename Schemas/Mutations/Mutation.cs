9114c43192ac14851b36c71ec66bc6bd9134d75f
Gabriel Liberato <gabrielliberato@Gabriels-MacBook-Pro.local>
2024-11-15 02:12:06 -0500

    Adding more types and scafolding project

--- Schemas/Mutations/Mutation.cs
+++ Schemas/Mutations/Mutation.cs
@@ -1,6 +1,3 @@
-using ticket_store_api.DTOs;
-using ticket_store_api.Services.Contracts;
-
 namespace ticket_store_api.Schemas.Mutations
 {
     public class Mutation
--- Schemas/Queries/Query.cs
+++ Schemas/Queries/Query.cs
@@ -1,10 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ticket_store_api.DTOs;
-using ticket_store_api.Services.Contracts;
-
 namespace ticket_store_api.Schemas.Queries
 {
     public class Query
